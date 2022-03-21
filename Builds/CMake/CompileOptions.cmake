0d7b39f83f0d2e9ee428d1a32f127de60212f019
snowapril <sinjihng@gmail.com>
2022-03-21 22:20:37 +0900

    setup: set default cxx compiler standard as c++20

--- Builds/CMake/CompileOptions.cmake
+++ Builds/CMake/CompileOptions.cmake
@@ -26,7 +26,7 @@ endif()
 #
 
 set(DEFAULT_PROJECT_OPTIONS
-	CXX_STANDARD              17 # Not available before CMake 3.8.2; see below for manual command line argument addition
+	CXX_STANDARD              20 # Not available before CMake 3.8.2; see below for manual command line argument addition
 	LINKER_LANGUAGE           "CXX"
 	POSITION_INDEPENDENT_CODE ON
 )
