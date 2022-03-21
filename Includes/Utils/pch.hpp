035127b8af38b8ca7d4f775630921c1f41a921a5
snowapril <sinjihng@gmail.com>
2022-03-21 22:25:11 +0900

    feat(vulkan_setup): add vulkan-hpp and glm to pch

--- Includes/Utils/pch.hpp
+++ Includes/Utils/pch.hpp
@@ -3,4 +3,22 @@
 #ifndef VOXEL_FLOW_PCH_HPP
 #define VOXEL_FLOW_PCH_HPP
 
+#define VK_NO_PROTOTYPES
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#define VULKAN_HPP_NO_EXCEPTIONS
+#define VULKAN_HPP_USE_REFLECT
+#include <vulkan/vulkan.hpp>
+
+#define GLM_FORCE_SWIZZLE
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_CTOR_INIT
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+
+namespace VoxelFlow 
+{
+
+}
+
 #endif
