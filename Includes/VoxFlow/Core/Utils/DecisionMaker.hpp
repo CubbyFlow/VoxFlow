c1cecc66c8abcc98b20f794b68b16663d75bb4a4
snowapril <sinjihng@gmail.com>
2022-04-04 11:42:27 +0900

    feat(vulkan_setup): add extension & layer picking decision functions

--- Includes/VoxFlow/Core/Utils/DecisionMaker.hpp
+++ Includes/VoxFlow/Core/Utils/DecisionMaker.hpp
@@ -14,6 +14,17 @@ class DecisionMaker
 {
  public:
     DecisionMaker() = delete;
+
+    [[nodiscard]] static VkResult pickLayers(
+        std::vector<const char*>& used,
+        const std::vector<VkLayerProperties>& properties,
+        const std::vector<Context::Entry>& requested);
+
+    [[nodiscard]] static VkResult pickExtensions(
+        std::vector<const char*>& used,
+        const std::vector<VkExtensionProperties>& properties,
+        const std::vector<Context::Entry>& requested,
+        std::vector<void*>& featureStructs);
 };
 }  // namespace VoxFlow
 
--- Sources/VoxFlow/Core/Utils/DecisionMaker.cpp
+++ Sources/VoxFlow/Core/Utils/DecisionMaker.cpp
@@ -5,4 +5,62 @@
 
 namespace VoxFlow
 {
+VkResult DecisionMaker::pickLayers(
+    std::vector<const char*>& used,
+    const std::vector<VkLayerProperties>& properties,
+    const std::vector<Context::Entry>& requested)
+{
+    for (const auto& entry : requested)
+    {
+        auto iter =
+            std::find_if(properties.begin(), properties.end(),
+                         [entry](const VkLayerProperties& p) {
+                             return strcmp(entry.entryName, p.layerName) == 0;
+                         });
+
+        if (iter != properties.end())
+        {
+            used.emplace_back(entry.entryName);
+        }
+        else if (entry.isOptional == false)
+        {
+            spdlog::error("Requested Vulkan Layer {} not found.",
+                          entry.entryName);
+            return VK_ERROR_LAYER_NOT_PRESENT;
+        }
+    }
+    return VK_SUCCESS;
 }
+
+VkResult DecisionMaker::pickExtensions(
+    std::vector<const char*>& used,
+    const std::vector<VkExtensionProperties>& properties,
+    const std::vector<Context::Entry>& requested,
+    std::vector<void*>& featureStructs)
+{
+    for (const auto& entry : requested)
+    {
+        auto iter = std::find_if(properties.begin(), properties.end(),
+                                 [entry](const VkExtensionProperties& p) {
+                                     return strcmp(entry.entryName,
+                                                   p.extensionName) == 0;
+                                 });
+
+        if (iter != properties.end())
+        {
+            used.emplace_back(entry.entryName);
+            if (entry.pFeatureStruct != nullptr)
+            {
+                featureStructs.push_back(entry.pFeatureStruct);
+            }
+        }
+        else if (entry.isOptional == false)
+        {
+            spdlog::error("Requested Vulkan Extension {} not found.",
+                          entry.entryName);
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+    }
+    return VK_SUCCESS;
+}
+}  // namespace VoxFlow
