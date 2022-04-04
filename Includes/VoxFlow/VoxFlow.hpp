7b80cb3a51143c69813bffdd7c49d9b314380bf6
snowapril <sinjihng@gmail.com>
2022-04-04 11:45:17 +0900

    feat(vulkan_setup): add voxflow include header

---
+++ Includes/VoxFlow/VoxFlow.hpp
@@ -0,0 +1,12 @@
+#ifndef VOX_FLOW_HPP
+#define
+
+#include <VoxFlow/Core/Devices/Context.hpp>
+#include <VoxFlow/Core/Devices/Instance.hpp>
+#include <VoxFlow/Core/Devices/LogicalDevice.hpp>
+#include <VoxFlow/Core/Devices/PhysicalDevice.hpp>
+#include <VoxFlow/Core/Utils/DecisionMaker.hpp>
+#include <VoxFlow/Core/Utils/Logger.hpp>
+#include <VoxFlow/Core/Utils/NonCopyable.hpp>
+
+#endif
