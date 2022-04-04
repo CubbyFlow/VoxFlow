080905712cddd32b47f60836fbda6434818416c1
snowapril <sinjihng@gmail.com>
2022-04-04 11:41:43 +0900

    feat(vulkan_setup): add vulkan resource decision making static class

---
+++ Includes/VoxFlow/Core/Utils/DecisionMaker.hpp
@@ -0,0 +1,20 @@
+// Author : snowapril
+
+#ifndef VOXEL_FLOW_DECISION_MAKER_HPP
+#define VOXEL_FLOW_DECISION_MAKER_HPP
+
+#include <VoxFlow/Core/Devices/Context.hpp>
+#include <VoxFlow/Core/Utils/pch.hpp>
+#include <string>
+#include <vector>
+
+namespace VoxFlow
+{
+class DecisionMaker
+{
+ public:
+    DecisionMaker() = delete;
+};
+}  // namespace VoxFlow
+
+#endif
---
+++ Sources/VoxFlow/Core/Utils/DecisionMaker.cpp
@@ -0,0 +1,8 @@
+// Author : snowapril
+
+#include <spdlog/spdlog.h>
+#include <VoxFlow/Core/Utils/DecisionMaker.hpp>
+
+namespace VoxFlow
+{
+}
