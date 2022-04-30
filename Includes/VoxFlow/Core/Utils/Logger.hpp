08cdbe4e5e8b0037ce7c120ba4569e8502d9df73
snowapril <sinjihng@gmail.com>
2022-04-30 18:11:05 +0900

    feat(vulkan_setup): add abort with log macro

--- Includes/VoxFlow/Core/Utils/Logger.hpp
+++ Includes/VoxFlow/Core/Utils/Logger.hpp
@@ -3,20 +3,33 @@
 #ifndef VOXEL_FLOW_LOGGER_HPP
 #define VOXEL_FLOW_LOGGER_HPP
 
-#include <cstdlib>
 #include <spdlog/spdlog.h>
+#include <cstdlib>
 
 namespace VoxFlow
 {
+#ifdef VOXFLOW_DEBUG
 #define VK_ASSERT(x)                                                     \
-	do                                                                   \
-	{                                                                    \
+    do                                                                   \
+    {                                                                    \
         if (!static_cast<bool>(x))                                       \
         {                                                                \
             spdlog::error("Vulkan Error at {}:{}.", __FILE__, __LINE__); \
             std::abort();                                                \
         }                                                                \
-	} while(false);
+    } while (false)
+#else
+#define VK_ASSERT(x) ((void)0)
+#endif
+
+#ifdef VOXFLOW_DEBUG
+#define ABORT_WITH_MSG(msg)                                        \
+    {                                                              \
+        spdlog::error("\"{}\" at {}:{}", msg, __FILE__, __LINE__); \
+    }
+#else
+#define ABORT_WITH_MSG(msg) ((void)msg)
+#endif
 }  // namespace VoxFlow
 
 #endif
