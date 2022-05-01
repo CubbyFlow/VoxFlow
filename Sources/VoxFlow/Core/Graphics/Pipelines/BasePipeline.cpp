b303509713a0ecb427c704938d708397c4db13e5
snowapril <sinjihng@gmail.com>
2022-05-01 23:23:53 +0900

    feat(vulkan_setup): add shader file utils using glslang third-party

--- Sources/VoxFlow/Core/Graphics/Pipelines/BasePipeline.cpp
+++ Sources/VoxFlow/Core/Graphics/Pipelines/BasePipeline.cpp
@@ -1,11 +1,19 @@
 // Author : snowapril
 
+#include <glslang_c_interface.h>
+#include <spdlog/spdlog.h>
 #include <VoxFlow/Core/Devices/LogicalDevice.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/BasePipeline.hpp>
+#include <VoxFlow/Core/Utils/Logger.hpp>
 #include <VoxFlow/Core/Utils/pch.hpp>
+#include <fstream>
+#include <string_view>
 
 namespace VoxFlow
 {
+glslang_stage_t getStageFromFilename(const std::string_view filename);
+bool readShaderFile(const std::string& filename, std::vector<char>* dst);
+
 BasePipeline::BasePipeline(const std::shared_ptr<LogicalDevice>& device)
     : _device(device)
 {
@@ -40,5 +48,43 @@ void BasePipeline::release()
         vkDestroyPipeline(_device->get(), _pipeline, nullptr);
         _pipeline = VK_NULL_HANDLE;
     }
+}
+
+glslang_stage_t getStageFromFilename(const std::string_view filename)
+{
+    if (filename.ends_with(".vert"))
+        return GLSLANG_STAGE_VERTEX;
+    if (filename.ends_with(".frag"))
+        return GLSLANG_STAGE_FRAGMENT;
+    if (filename.ends_with(".geom"))
+        return GLSLANG_STAGE_GEOMETRY;
+    if (filename.ends_with(".comp"))
+        return GLSLANG_STAGE_COMPUTE;
+    if (filename.ends_with(".tesc"))
+        return GLSLANG_STAGE_TESSCONTROL;
+    if (filename.ends_with(".tese"))
+        return GLSLANG_STAGE_TESSEVALUATION;
+
+    ABORT_WITH_MSG("Undefined shader extension is given");
+    return GLSLANG_STAGE_COUNT;
+}
+
+bool readShaderFile(const std::string& filename, std::vector<char>* dst)
+{
+    std::ifstream file(filename, std::ios::in | std::ios::ate);
+    if (!file.is_open())
+    {
+        ABORT_WITH_MSG("Failed to find shader file {}", filename);
+        return false;
+    }
+
+    const size_t fileSize = file.tellg();
+    dst->resize(fileSize);
+
+    file.seekg(std::ios::beg);
+    file.read(dst->data(), static_cast<std::streamsize>(fileSize));
+
+    file.close();
+    return true;
 }
 }  // namespace VoxFlow
