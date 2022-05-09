03871fb9cecf8a5b45cdec29ecd2c362cc47d73e
snowapril <sinjihng@gmail.com>
2022-05-09 16:17:10 +0900

    test(vulkan_setup): pass valid pipeline layout to test instead of null handle

--- Tests/UnitTests/Core/Graphics/Pipelines/ComputePipelineTests.cpp
+++ Tests/UnitTests/Core/Graphics/Pipelines/ComputePipelineTests.cpp
@@ -3,6 +3,7 @@
 #include <VoxFlow/Core/Devices/Instance.hpp>
 #include <VoxFlow/Core/Devices/LogicalDevice.hpp>
 #include <VoxFlow/Core/Devices/PhysicalDevice.hpp>
+#include <VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/ComputePipeline.hpp>
 #include "../../../UnitTestUtils.hpp"
 
@@ -14,9 +15,12 @@ TEST_CASE("Vulkan Compute Pipeline Initialization")
     auto logicalDevice = std::make_shared<VoxFlow::LogicalDevice>(
         gVulkanContext, physicalDevice);
 
+    auto pipelineLayout =
+        std::make_shared<VoxFlow::PipelineLayout>(logicalDevice);
+
     VoxFlow::ComputePipeline testPipeline(
         logicalDevice, { RESOURCES_DIR "/Shaders/test_shader.comp" },
-        VK_NULL_HANDLE);
+        pipelineLayout);
 
     CHECK_NE(testPipeline.get(), VK_NULL_HANDLE);
 }
--- Tests/UnitTests/Core/Graphics/Pipelines/GraphicsPipelineTests.cpp
+++ Tests/UnitTests/Core/Graphics/Pipelines/GraphicsPipelineTests.cpp
@@ -4,21 +4,25 @@
 #include <VoxFlow/Core/Devices/LogicalDevice.hpp>
 #include <VoxFlow/Core/Devices/PhysicalDevice.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/GraphicsPipeline.hpp>
+#include <VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp>
 #include "../../../UnitTestUtils.hpp"
 
 TEST_CASE("Vulkan Graphics Pipeline Initialization")
 {
-    VoxFlow::Instance instance(gVulkanContext);
+    const VoxFlow::Instance instance(gVulkanContext);
     VoxFlow::PhysicalDevice physicalDevice(instance);
 
     auto logicalDevice = std::make_shared<VoxFlow::LogicalDevice>(
         gVulkanContext, physicalDevice);
 
-    VoxFlow::GraphicsPipeline testPipeline(
+    const auto pipelineLayout =
+        std::make_shared<VoxFlow::PipelineLayout>(logicalDevice);
+
+    const VoxFlow::GraphicsPipeline testPipeline(
         logicalDevice,
         { RESOURCES_DIR "/Shaders/test_shader.vert",
           RESOURCES_DIR "/Shaders/test_shader.frag" },
-        VoxFlow::PipelineCreateInfo::CreateDefault(), VK_NULL_HANDLE);
+        VoxFlow::PipelineCreateInfo::CreateDefault(), pipelineLayout);
 
     CHECK_NE(testPipeline.get(), VK_NULL_HANDLE);
 }
