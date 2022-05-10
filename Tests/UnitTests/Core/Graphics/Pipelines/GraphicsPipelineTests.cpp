1f1a02549d8ce9921dc89e943adefa8271c45cb3
snowapril <sinjihng@gmail.com>
2022-05-10 11:57:55 +0900

    test(vulkan_setup): pass renderpass, viewport, scissor to pipeline info

--- Tests/UnitTests/Core/Graphics/Pipelines/GraphicsPipelineTests.cpp
+++ Tests/UnitTests/Core/Graphics/Pipelines/GraphicsPipelineTests.cpp
@@ -5,6 +5,7 @@
 #include <VoxFlow/Core/Devices/PhysicalDevice.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/GraphicsPipeline.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp>
+#include <VoxFlow/Core/Graphics/RenderPass/RenderPass.hpp>
 #include "../../../UnitTestUtils.hpp"
 
 TEST_CASE("Vulkan Graphics Pipeline Initialization")
@@ -18,11 +19,29 @@ TEST_CASE("Vulkan Graphics Pipeline Initialization")
     const auto pipelineLayout =
         std::make_shared<VoxFlow::PipelineLayout>(logicalDevice);
 
+    const VoxFlow::RenderPass renderPass(logicalDevice);
+
+    const VkViewport viewport = { .x = 0.0f,
+                                  .y = 0.0f,
+                                  .width = 1.0f,
+                                  .height = 1.0f,
+                                  .minDepth = 0.0f,
+                                  .maxDepth = 1.0f };
+    const VkRect2D scissor = { .offset = { .x = 0, .y = 0 },
+                               .extent = { .width = 1u, .height = 1u } };
+
+    auto pipelineCreateInfo = VoxFlow::PipelineCreateInfo::CreateDefault();
+    pipelineCreateInfo.renderPass = renderPass.get();
+    pipelineCreateInfo.viewportState.viewportCount = 1;
+    pipelineCreateInfo.viewportState.scissorCount = 1;
+    pipelineCreateInfo.viewportState.pViewports = &viewport;
+    pipelineCreateInfo.viewportState.pScissors = &scissor;
+
     const VoxFlow::GraphicsPipeline testPipeline(
         logicalDevice,
         { RESOURCES_DIR "/Shaders/test_shader.vert",
           RESOURCES_DIR "/Shaders/test_shader.frag" },
-        VoxFlow::PipelineCreateInfo::CreateDefault(), pipelineLayout);
+        pipelineCreateInfo, pipelineLayout);
 
     CHECK_NE(testPipeline.get(), VK_NULL_HANDLE);
 }
