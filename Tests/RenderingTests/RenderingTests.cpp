65d98288abb1cd9511e00047acba282fe3fb8f7f
snowapril <sinjihng@gmail.com>
2023-01-24 22:57:28 +0900

    setup: add initiali rendering tests

---
+++ Tests/RenderingTests/RenderingTests.cpp
@@ -0,0 +1,21 @@
+// Author : snowapril
+
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include "UnitTestUtils.hpp"
+
+VoxFlow::Context gVulkanContext;
+
+int main()
+{
+    doctest::Context context;
+
+    gVulkanContext.setVersion(1, 3);
+    gVulkanContext.addRequiredQueue(
+        "GCT",
+        VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT | VK_QUEUE_TRANSFER_BIT, 1,
+        1.0F);
+
+    // Run queries, or run tests unless --no-run is specified
+    return context.run();
+}
