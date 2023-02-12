b041c2fc3d4c72487c7b11b2e1b3fb6fdc57d2d6
snowapril <sinjihng@gmail.com>
2023-02-12 21:14:53 +0900

    feat : implement CommandBuffer begin/end with empty recorded commands

--- Includes/VoxFlow/Core/Graphics/Commands/CommandBuffer.hpp
+++ Includes/VoxFlow/Core/Graphics/Commands/CommandBuffer.hpp
@@ -5,25 +5,46 @@
 
 #include <volk/volk.h>
 #include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <VoxFlow/Core/Utils/FenceObject.hpp>
+#include <string>
 
 namespace VoxFlow
 {
-class CommandBuffer : NonCopyable
+class Queue;
+class CommandPool;
+class RenderPass;
+class SwapChain;
+
+class CommandBuffer : private NonCopyable
 {
  public:
-    explicit CommandBuffer(VkCommandBufferLevel cmdBufferLevel,
-                           VkQueueFlagBits queueType,
-                           bool instantBegin = false);
+    explicit CommandBuffer(Queue* commandQueue, CommandPool* commandPool, VkCommandBuffer vkCommandBuffer);
     ~CommandBuffer();
 
     [[nodiscard]] VkCommandBuffer get() const noexcept
     {
-        return _commandBuffer;
+        return _vkCommandBuffer;
+    }
+
+    // Begin command buffer to record new commands
+    void beginCommandBuffer(uint32_t swapChainIndex, uint32_t frameIndex, const std::string& debugName);
+    
+    // End command buffer recording
+    void endCommandBuffer();
+    inline const FenceObject& getFenceToSignal() const
+    {
+        return _fenceToSignal;
     }
 
  private:
-    VkCommandBuffer _commandBuffer{ VK_NULL_HANDLE };
+    Queue* _commandQueue = nullptr;
     VkQueueFlagBits _queueType;
+    FenceObject _fenceToSignal;
+    VkCommandBuffer _vkCommandBuffer = VK_NULL_HANDLE;
+    uint32_t _swapChainIndexCached = UINT32_MAX;
+    uint32_t _frameIndexCached = UINT32_MAX;
+    std::string _debugName;
+    bool _hasBegun = false;
 };
 }  // namespace VoxFlow
 
--- Sources/VoxFlow/Core/Graphics/Commands/CommandBuffer.cpp
+++ Sources/VoxFlow/Core/Graphics/Commands/CommandBuffer.cpp
@@ -1,22 +1,55 @@
 // Author : snowapril
 
+#include <VoxFlow/Core/Devices/Queue.hpp>
+#include <VoxFlow/Core/Devices/SwapChain.hpp>
 #include <VoxFlow/Core/Graphics/Commands/CommandBuffer.hpp>
+#include <VoxFlow/Core/Graphics/Commands/CommandPool.hpp>
+#include <VoxFlow/Core/Graphics/RenderPass/FrameBuffer.hpp>
+#include <VoxFlow/Core/Utils/Logger.hpp>
 
 namespace VoxFlow
 {
-CommandBuffer::CommandBuffer(VkCommandBufferLevel cmdBufferLevel,
-                             VkQueueFlagBits queueType,
-                             bool instantBegin /* false */)
-    : _queueType(queueType)
+CommandBuffer::CommandBuffer(Queue* commandQueue, CommandPool* commandPool,
+                             VkCommandBuffer vkCommandBuffer)
+    : _commandQueue(commandQueue),
+      // _ownerCommandPool(commandPool),
+      _vkCommandBuffer(vkCommandBuffer)
 {
-    // Do nothing
-    (void)cmdBufferLevel;
-    (void)instantBegin;
-    (void)_queueType;
+    (void)commandPool;
 }
 CommandBuffer::~CommandBuffer()
 {
     // Do nothing
 }
 
+void CommandBuffer::beginCommandBuffer(uint32_t swapChainIndex,
+                                       uint32_t frameIndex,
+                                       const std::string& debugName)
+{
+    _swapChainIndexCached = swapChainIndex;
+    _frameIndexCached = frameIndex;
+    _debugName = debugName;
+
+    _fenceToSignal = FenceObject(
+        _commandQueue, _commandQueue->getLastExecutedFenceValue() + 1);
+
+    VOX_ASSERT(_hasBegun == false,
+               "Duplicated beginning on the same CommandBuffer(%s)",
+               _debugName);
+
+    VkCommandBufferBeginInfo beginInfo = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+        .pNext = nullptr,
+        .flags = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        .pInheritanceInfo = nullptr,
+    };
+    vkBeginCommandBuffer(_vkCommandBuffer, &beginInfo);
+    _hasBegun = true;
+}
+
+void CommandBuffer::endCommandBuffer()
+{
+    vkEndCommandBuffer(_vkCommandBuffer);
+    _hasBegun = false;
+}
 }  // namespace VoxFlow
