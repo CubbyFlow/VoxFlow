1d802b5ef77ef26c93aa5f02939e165756c3bcb5
snowapril <sinjihng@gmail.com>
2023-02-12 21:17:47 +0900

    feat : implement Queue submission and synchronizing with semaphore

--- Includes/VoxFlow/Core/Devices/Queue.hpp
+++ Includes/VoxFlow/Core/Devices/Queue.hpp
@@ -4,34 +4,91 @@
 #define VOXEL_FLOW_QUEUE_HPP
 
 #include <volk/volk.h>
+#include <VoxFlow/Core/Utils/FenceObject.hpp>
+#include <VoxFlow/Core/Utils/Logger.hpp>
 #include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <memory>
+#include <string>
+#include <vector>
 
 namespace VoxFlow
 {
 class LogicalDevice;
+class CommandBuffer;
+class SwapChain;
 
-class Queue : NonCopyable
+class Queue : private NonCopyable
 {
  public:
-    explicit Queue(VkQueue queueHandle, uint32_t familyIndex,
+    explicit Queue(const std::string& debugName, LogicalDevice* logicalDevice, VkQueueFlags queueTypeFlags,
+                   VkQueue _queueTypeBits, uint32_t familyIndex,
                    uint32_t queueIndex) noexcept;
-    ~Queue() override = default;
+    ~Queue();
     Queue(Queue&& other) noexcept;
     Queue& operator=(Queue&& other) noexcept;
 
-    [[nodiscard]] VkQueue get() const noexcept
+    [[nodiscard]] inline VkQueue get() const noexcept
     {
         return _queue;
     }
-    [[nodiscard]] unsigned int getFamilyIndex() const noexcept
+
+    [[nodiscard]] inline unsigned int getFamilyIndex() const noexcept
     {
         return _familyIndex;
     }
 
+    inline const std::string& getDebugName() const
+    {
+        return _debugName;
+    }
+
+    // Submit given single command buffer to queue and returns FenceObject for
+    // waiting submission completed
+    FenceObject submitCommandBuffer(
+        const std::shared_ptr<CommandBuffer>& commandBuffer,
+        const std::shared_ptr<SwapChain>& swapChain, const uint32_t frameIndex,
+        const bool waitCompletion);
+
+    // Submit given command buffers to queue and returns FenceObject for waiting
+    // submission completed
+    FenceObject submitCommandBufferBatch(
+        const std::vector<std::shared_ptr<CommandBuffer>>&
+            batchedCommandBuffers,
+        const std::shared_ptr<SwapChain>& swapChain, const uint32_t frameIndex,
+        const bool waitAllCompletion);
+
+    // Returns Timeline semaphore which synchronized with queue submission
+    [[nodiscard]] inline VkSemaphore* getSubmitTimelineSemaphore()
+    {
+        return &_submitTimelineSemaphore;
+    }
+
+    // Returns last executed FenceValue of Timeline semaphore
+    [[nodiscard]] inline uint64_t getLastExecutedFenceValue() const
+    {
+        return _lastExecutedFence.getFenceValue();
+    }
+
+    // Returns last signaled FenceValue of Timeline semaphore
+    [[nodiscard]] inline uint64_t getLastCompletedFenceValue() const
+    {
+        return _lastCompletedFence.getFenceValue();
+    }
+
+    // Returns Timeline semaphore's value which synchronized with queue
+    // submission
+    [[nodiscard]] uint64_t getTimelineSemaphoreValue();
+
  private:
+    std::string _debugName;
+    LogicalDevice* _logicalDevice{ nullptr };
+    VkQueueFlags _queueTypeFlags;
+    VkQueue _queue{ VK_NULL_HANDLE };
     uint32_t _familyIndex{ 0 };
     uint32_t _queueIndex{ 0 };
-    VkQueue _queue{ VK_NULL_HANDLE };
+    VkSemaphore _submitTimelineSemaphore{ VK_NULL_HANDLE };
+    FenceObject _lastExecutedFence;
+    FenceObject _lastCompletedFence;
 };
 }  // namespace VoxFlow
 
--- Sources/VoxFlow/Core/Devices/Queue.cpp
+++ Sources/VoxFlow/Core/Devices/Queue.cpp
@@ -1,32 +1,185 @@
 // Author : snowapril
 
+#include <VoxFlow/Core/Devices/LogicalDevice.hpp>
 #include <VoxFlow/Core/Devices/Queue.hpp>
+#include <VoxFlow/Core/Devices/SwapChain.hpp>
+#include <VoxFlow/Core/Graphics/Commands/CommandBuffer.hpp>
+#include <VoxFlow/Core/Utils/Initializer.hpp>
+#include <VoxFlow/Core/Utils/DebugUtil.hpp>
 #include <VoxFlow/Core/Utils/Logger.hpp>
+#include <VoxFlow/Core/Utils/RendererCommon.hpp>
 
 namespace VoxFlow
 {
-Queue::Queue(VkQueue queueHandle, uint32_t familyIndex,
-             uint32_t queueIndex) noexcept
-    : _queue(queueHandle), _familyIndex(familyIndex), _queueIndex(queueIndex)
+Queue::Queue(const std::string& debugName, LogicalDevice* logicalDevice,
+             VkQueueFlags queueTypeFlags, VkQueue queueHandle,
+             uint32_t familyIndex, uint32_t queueIndex) noexcept
+    : _debugName(debugName),
+      _logicalDevice(logicalDevice),
+      _queueTypeFlags(queueTypeFlags),
+      _queue(queueHandle),
+      _familyIndex(familyIndex),
+      _queueIndex(queueIndex),
+      _lastExecutedFence(this, 0ULL),
+      _lastCompletedFence(this, 0ULL)
 {
-    // DO NOTHING
+    DebugUtil::setObjectName(_logicalDevice, queueHandle, _debugName.c_str());
+
+    VkSemaphoreTypeCreateInfo timelineCreateInfo;
+    timelineCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO;
+    timelineCreateInfo.pNext = NULL;
+    timelineCreateInfo.semaphoreType = VK_SEMAPHORE_TYPE_TIMELINE;
+    timelineCreateInfo.initialValue = 0;
+
+    VkSemaphoreCreateInfo createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+    createInfo.pNext = &timelineCreateInfo;
+    createInfo.flags = 0;
+
+    vkCreateSemaphore(_logicalDevice->get(), &createInfo, NULL,
+                      &_submitTimelineSemaphore);
+}
+
+Queue::~Queue()
+{
+    vkQueueWaitIdle(_queue);
+    vkDestroySemaphore(_logicalDevice->get(), _submitTimelineSemaphore,
+                       nullptr);
 }
 
 Queue::Queue(Queue&& other) noexcept
-    : _queue(std::move(other._queue)),
-      _familyIndex(other._familyIndex),
-      _queueIndex(other._queueIndex)
 {
+    operator=(std::move(other));
 }
 
 Queue& Queue::operator=(Queue&& other) noexcept
 {
     if (this != &other)
     {
-        _queue = std::move(other._queue);
+        _queue = other._queue;
         _familyIndex = other._familyIndex;
         _queueIndex = other._queueIndex;
+        _logicalDevice = other._logicalDevice;
+        _submitTimelineSemaphore = other._submitTimelineSemaphore;
+
+        other._queue = VK_NULL_HANDLE;
+        other._submitTimelineSemaphore = VK_NULL_HANDLE;
     }
     return *this;
 }
+
+FenceObject Queue::submitCommandBuffer(
+    const std::shared_ptr<CommandBuffer>& commandBuffer,
+    const std::shared_ptr<SwapChain>& swapChain, const uint32_t frameIndex,
+    const bool waitCompletion)
+{
+    VOX_ASSERT((swapChain == nullptr) || (frameIndex < FRAME_BUFFER_COUNT),
+               "Must provide valid frame index when swapChain is not nullptr");
+
+    VkCommandBuffer cmdBufferToSubmit = commandBuffer->get();
+
+    uint64_t signalingValues[2] = {
+        commandBuffer->getFenceToSignal().getFenceValue(), 0ULL
+    };
+
+    uint64_t waitingValues[2] = { signalingValues[0] - 1, 0ULL };
+    VkTimelineSemaphoreSubmitInfo timelineInfo{
+        .sType = VK_STRUCTURE_TYPE_TIMELINE_SEMAPHORE_SUBMIT_INFO,
+        .pNext = NULL,
+        .waitSemaphoreValueCount = swapChain == nullptr ? 1U : 2U,  // TODO
+        .pWaitSemaphoreValues = waitingValues,                      // TODO
+        .signalSemaphoreValueCount = swapChain == nullptr ? 1U : 2U,
+        .pSignalSemaphoreValues = signalingValues,
+    };
+
+    VkSemaphore waitingSemaphores[2] = {
+        _submitTimelineSemaphore,
+        swapChain == nullptr ? VK_NULL_HANDLE
+                             : swapChain->getCurrentBackBufferReadySemaphore()
+    };
+
+    VkSemaphore signalingSemaphores[2] = {
+        _submitTimelineSemaphore,
+        swapChain == nullptr ? VK_NULL_HANDLE
+                             : swapChain->getCurrentPresentReadySemaphore()
+    };
+
+    // TODO(snowapril) : modify stage masks
+    VkPipelineStageFlags waitDstStageMasks[2] = {
+        VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+        VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT
+    };
+
+    // Add wait semaphore for waiting acquire back buffer image
+    VkSubmitInfo submitInfo = { .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+                                .pNext = &timelineInfo,
+                                .waitSemaphoreCount =
+                                    swapChain != nullptr ? 2U : 1U,
+                                .pWaitSemaphores = &waitingSemaphores[0],
+                                .pWaitDstStageMask = waitDstStageMasks,
+                                .commandBufferCount = 1,
+                                .pCommandBuffers = &cmdBufferToSubmit,
+                                .signalSemaphoreCount =
+                                    swapChain != nullptr ? 2U : 1U,
+                                .pSignalSemaphores = &signalingSemaphores[0] };
+
+    _lastExecutedFence = FenceObject(this, waitingValues[0]);
+
+    VK_ASSERT(vkQueueSubmit(_queue, 1, &submitInfo, VK_NULL_HANDLE));
+
+    _lastExecutedFence.advanceFenceValue();
+
+    if (waitCompletion)
+    {
+        VkSemaphoreWaitInfo waitInfo{
+            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO,
+            .pNext = NULL,
+            .flags = 0,
+            .semaphoreCount = 1,
+            .pSemaphores = &_submitTimelineSemaphore,
+            .pValues = signalingValues,
+        };
+
+        vkWaitSemaphoresKHR(_logicalDevice->get(), &waitInfo, UINT64_MAX);
+        _lastCompletedFence = FenceObject(this, signalingValues[0]);
+    }
+    else if (swapChain != nullptr)
+    {
+        swapChain->addWaitSemaphores(frameIndex, _submitTimelineSemaphore,
+                                     signalingValues[0]);
+    }
+
+    return _lastExecutedFence;
+}
+
+FenceObject Queue::submitCommandBufferBatch(
+    const std::vector<std::shared_ptr<CommandBuffer>>& batchedCommandBuffers,
+    const std::shared_ptr<SwapChain>& swapChain, const uint32_t frameIndex,
+    const bool waitAllCompletion)
+{
+    (void)batchedCommandBuffers;
+    (void)swapChain;
+    (void)frameIndex;
+    (void)waitAllCompletion;
+    VOX_ASSERT(false, "Not Implemented Yet");
+    return _lastExecutedFence;
+}
+
+uint64_t Queue::getTimelineSemaphoreValue()
+{
+    uint64_t value = 0;
+    VK_ASSERT(vkGetSemaphoreCounterValueKHR(_logicalDevice->get(),
+                                            _submitTimelineSemaphore, &value));
+
+    if (value == UINT64_MAX)
+    {
+        // As returning UINT64_MAX means current logical device is lost,
+        // call its handler.
+        DeviceRemoveTracker::get()->onDeviceRemoved();
+    }
+
+    _lastCompletedFence = FenceObject(this, value);
+    return value;
+}
+
 }  // namespace VoxFlow
