560d2464fad337d6dbe49c90de081f8f1a38b323
snowapril <sinjihng@gmail.com>
2023-02-12 21:07:51 +0900

    feat : modify PipelineLayout implementation to take DescriptorSetLayouts and create VkPipelineLayout handle

--- Includes/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp
+++ Includes/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp
@@ -5,15 +5,20 @@
 
 #include <volk/volk.h>
 #include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <memory>
+#include <vector>
 
 namespace VoxFlow
 {
 class LogicalDevice;
+class DescriptorSetLayout;
 
 class PipelineLayout : NonCopyable
 {
  public:
-    explicit PipelineLayout(const std::shared_ptr<LogicalDevice>& device);
+    explicit PipelineLayout(
+        LogicalDevice* logicalDevice,
+        const std::vector<std::shared_ptr<DescriptorSetLayout>>& setLayouts);
     ~PipelineLayout() override;
     PipelineLayout(PipelineLayout&& other) noexcept;
     PipelineLayout& operator=(PipelineLayout&& other) noexcept;
@@ -27,8 +32,9 @@ class PipelineLayout : NonCopyable
     void release();
 
  private:
-    std::shared_ptr<LogicalDevice> _device;
+    LogicalDevice* _logicalDevice;
     VkPipelineLayout _layout{ VK_NULL_HANDLE };
+    std::vector<std::shared_ptr<DescriptorSetLayout>> _setLayouts;
 };
 }  // namespace VoxFlow
 
