7a9d7b817ee9531aa4c8fd444ff02742b7b5d494
snowapril <sinjihng@gmail.com>
2023-05-31 23:42:46 +0900

    feat : modify pipeline layout to manage descriptor set allocator for each module

--- Includes/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp
+++ Includes/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp
@@ -5,36 +5,44 @@
 
 #include <volk/volk.h>
 #include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <VoxFlow/Core/Graphics/Pipelines/ShaderLayoutBinding.hpp>
 #include <memory>
 #include <vector>
+#include <array>
 
 namespace VoxFlow
 {
 class LogicalDevice;
-class DescriptorSetLayout;
+class DescriptorSetAllocator;
 
 class PipelineLayout : NonCopyable
 {
  public:
     explicit PipelineLayout(
         LogicalDevice* logicalDevice,
-        const std::vector<std::shared_ptr<DescriptorSetLayout>>& setLayouts);
+        std::vector<ShaderLayoutBinding>&& setLayoutBindings);
     ~PipelineLayout() override;
     PipelineLayout(PipelineLayout&& other) noexcept;
     PipelineLayout& operator=(PipelineLayout&& other) noexcept;
 
     [[nodiscard]] VkPipelineLayout get() const noexcept
     {
-        return _layout;
+        return _vkPipelineLayout;
+    }
+
+    [[nodiscard]] DescriptorSetAllocator* getDescSetAllocator(const SetSlotCategory category) const
+    {
+        return _setAllocators[static_cast<uint32_t>(category)].get();
     }
 
  protected:
     void release();
 
  private:
-    LogicalDevice* _logicalDevice;
-    VkPipelineLayout _layout{ VK_NULL_HANDLE };
-    std::vector<std::shared_ptr<DescriptorSetLayout>> _setLayouts;
+    LogicalDevice* _logicalDevice = nullptr;
+    VkPipelineLayout _vkPipelineLayout{ VK_NULL_HANDLE };
+    std::array<std::shared_ptr<DescriptorSetAllocator>, MAX_NUM_SET_SLOTS> _setAllocators;
+    std::vector<ShaderLayoutBinding> _setLayoutBindings;
 };
 }  // namespace VoxFlow
 
--- Sources/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.cpp
+++ Sources/VoxFlow/Core/Graphics/Pipelines/PipelineLayout.cpp
@@ -1,7 +1,8 @@
 // Author : snowapril
 
 #include <VoxFlow/Core/Devices/LogicalDevice.hpp>
-#include <VoxFlow/Core/Graphics/Descriptors/DescriptorSetLayout.hpp>
+#include <VoxFlow/Core/Graphics/Descriptors/DescriptorSetAllocator.hpp>
+#include <VoxFlow/Core/Graphics/Descriptors/DescriptorSetAllocatorPool.hpp>
 #include <VoxFlow/Core/Graphics/Pipelines/PipelineLayout.hpp>
 #include <VoxFlow/Core/Utils/Logger.hpp>
 #include <algorithm>
@@ -10,25 +11,69 @@ namespace VoxFlow
 {
 PipelineLayout::PipelineLayout(
     LogicalDevice* logicalDevice,
-    const std::vector<std::shared_ptr<DescriptorSetLayout>>& setLayouts)
-    : _logicalDevice(logicalDevice), _setLayouts(setLayouts)
+    std::vector<ShaderLayoutBinding>&& setLayoutBindings)
+    : _logicalDevice(logicalDevice), _setLayoutBindings(setLayoutBindings)
 {
-    auto layoutInfo = Initializer::MakeInfo<VkPipelineLayoutCreateInfo>();
+    std::array<DescriptorSetLayoutDesc, MAX_NUM_SET_SLOTS> combinedSetLayouts;
+    for (const ShaderLayoutBinding& shaderBinding : setLayoutBindings)
+    {
+        for (uint32_t set = 0; set < MAX_NUM_SET_SLOTS; ++set)
+        {
+            std::for_each(
+                shaderBinding._sets[set]._bindingMap.begin(),
+                shaderBinding._sets[set]._bindingMap.end(),
+                [&combinedSetLayouts,
+                 set](const DescriptorSetLayoutDesc::ContainerType::value_type&
+                          bindingPair) {
+                    DescriptorSetLayoutDesc::ContainerType::const_iterator it =
+                        combinedSetLayouts[set]._bindingMap.find(
+                            bindingPair.first);
+                    if (it != combinedSetLayouts[set]._bindingMap.end())
+                    {
+                        // TODO(snowapril) : Check given resource is collided
+                        // with already collected one.
+                    }
+                    else
+                    {
+                        combinedSetLayouts[set]._bindingMap.emplace(
+                            bindingPair);
+                    }
+                });
+                
+            combinedSetLayouts[set]._stageFlags |=
+                shaderBinding._sets[set]._stageFlags;
+        }
+    }
+
+    DescriptorSetAllocatorPool* descriptorSetAllocatorPool =
+        _logicalDevice->getDescriptorSetAllocatorPool();
 
     std::vector<VkDescriptorSetLayout> vkSetLayouts;
-    vkSetLayouts.reserve(_setLayouts.size());
+    for (uint32_t set = 0; set < MAX_NUM_SET_SLOTS; ++set)
+    {
+        if (combinedSetLayouts[set]._stageFlags != 0)
+        {
+            _setAllocators[set] =
+                descriptorSetAllocatorPool->getOrCreateDescriptorSetAllocator(
+                    combinedSetLayouts[set]);
+            vkSetLayouts.push_back(
+                _setAllocators[set]->getVkDescriptorSetLayout());
+        }
+    }
 
-    std::for_each(
-        _setLayouts.begin(), _setLayouts.end(),
-        [&vkSetLayouts](const std::shared_ptr<DescriptorSetLayout>& setLayout) {
-            vkSetLayouts.push_back(setLayout->get());
-        });
+    // TODO(snowapril) : implement push constants
+    VkPipelineLayoutCreateInfo createInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .setLayoutCount = static_cast<uint32_t>(vkSetLayouts.size()),
+        .pSetLayouts = vkSetLayouts.data(),
+        .pushConstantRangeCount = 0,
+        .pPushConstantRanges = nullptr,
+    };
 
-    layoutInfo.setLayoutCount = static_cast<uint32_t>(vkSetLayouts.size());
-    layoutInfo.pSetLayouts = vkSetLayouts.data();
-
-    VK_ASSERT(vkCreatePipelineLayout(_logicalDevice->get(), &layoutInfo,
-                                     nullptr, &_layout));
+    VK_ASSERT(vkCreatePipelineLayout(_logicalDevice->get(), &createInfo,
+                                     nullptr, &_vkPipelineLayout));
 }
 
 PipelineLayout::~PipelineLayout()
@@ -37,9 +82,8 @@ PipelineLayout::~PipelineLayout()
 }
 
 PipelineLayout::PipelineLayout(PipelineLayout&& other) noexcept
-    : _logicalDevice(std::move(other._logicalDevice)), _layout(other._layout)
 {
-    // Do nothing
+    operator=(std::move(other));
 }
 
 PipelineLayout& PipelineLayout::operator=(PipelineLayout&& other) noexcept
@@ -47,17 +91,19 @@ PipelineLayout& PipelineLayout::operator=(PipelineLayout&& other) noexcept
     if (&other != this)
     {
         _logicalDevice = std::move(other._logicalDevice);
-        _layout = other._layout;
+        _vkPipelineLayout = other._vkPipelineLayout;
+        _setAllocators.swap(other._setAllocators);
+        _setLayoutBindings.swap(other._setLayoutBindings);
     }
     return *this;
 }
 
 void PipelineLayout::release()
 {
-    if (_layout)
+    if (_vkPipelineLayout)
     {
-        vkDestroyPipelineLayout(_logicalDevice->get(), _layout, nullptr);
-        _layout = VK_NULL_HANDLE;
+        vkDestroyPipelineLayout(_logicalDevice->get(), _vkPipelineLayout, nullptr);
+        _vkPipelineLayout = VK_NULL_HANDLE;
     }
 }
 }  // namespace VoxFlow
