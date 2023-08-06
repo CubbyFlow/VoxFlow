73d420fe2febb9bb63ffa3366c4618ab5e54737c
snowapril <sinjihng@gmail.com>
2023-08-06 18:22:42 +0900

    feat : add initial resource lifetime & synchronization tracker

---
+++ Includes/VoxFlow/Core/Resources/ResourceTracker.hpp
@@ -0,0 +1,105 @@
+// Author : snowapril
+
+#ifndef VOXEL_FLOW_RESOURCE_TRACKER_HPP
+#define VOXEL_FLOW_RESOURCE_TRACKER_HPP
+
+#include <VoxFlow/Core/Utils/FenceObject.hpp>
+#include <VoxFlow/Core/Utils/Handle.hpp>
+#include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <VoxFlow/Core/Utils/RendererCommon.hpp>
+#include <string_view>
+#include <unordered_map>
+
+namespace VoxFlow
+{
+class LogicalDevice;
+class RenderResourceMemoryPool;
+
+class ResourceTracker : private NonCopyable
+{
+ public:
+    static ResourceTracker& This()
+    {
+        static ResourceTracker sResourceTracker;
+        return sResourceTracker;
+    }
+
+    void release()
+    {
+        _renderResources.clear();
+    }
+
+ public:
+    template <typename RenderResourceType>
+    Handle<RenderResourceType> allocate(std::string_view&& debugName,
+                                        LogicalDevice* logicalDevice,
+                                        RenderResourceMemoryPool* memoryPool)
+    {
+        std::unique_ptr<RenderResource> renderResource =
+            std::make_unique<RenderResourceType>(std::move(debugName),
+                                                 logicalDevice, memoryPool);
+
+        Handle resourceHandle(renderResource);
+        HandleBase::HandleID handleID = resourceHandle.getHandleID();
+
+        auto iter = _renderResources.find(handleID);
+        if (iter != _renderResources.end())
+        {
+            VOX_ASSERT(false, "Duplicate HandleID must not be exist");
+        }
+        else
+        {
+            _renderResources.insert(handleID, std::move(renderResource));
+        }
+
+        return std::move(resourceHandle);
+    }
+
+    void deallocate(HandleBase::HandleID handleID)
+    {
+        // TODO(snowapril) : refCount manage
+        auto iter = _renderResources.find(handleID);
+        if (iter != _renderResources.end())
+        {
+            _renderResources.erase(iter);
+        }
+        else
+        {
+            VOX_ASSERT(false, "Unknown handleID must not be inserted");
+        }
+    }
+
+    void setFenceObject(HandleBase::HandleID handleID, FenceObject fenceObject)
+    {
+        // TODO(snowapril) : need simultaneous access tracking implement
+        (void)handleID;
+        (void)fenceObject;
+    }
+
+ private:
+    std::unordered_map<HandleBase::HandleID, std::unique_ptr<RenderResource>>
+        _renderResources;
+
+ private:
+    ResourceTracker(){};
+    ~ResourceTracker()
+    {
+        release();
+    };
+    ResourceTracker(ResourceTracker&& rhs)
+    {
+        operator=(std::move(rhs));
+    }
+    ResourceTracker& operator=(ResourceTracker&& rhs)
+    {
+        if (this != &rhs)
+        {
+            _renderResources.swap(rhs._renderResources);
+        }
+        return *this;
+    }
+};
+
+}  // namespace VoxFlow
+
+#endif
--- Includes/VoxFlow/Core/Utils/Handle.hpp
+++ Includes/VoxFlow/Core/Utils/Handle.hpp
@@ -3,7 +3,9 @@
 #ifndef VOXEL_FLOW_HANDLE_HPP
 #define VOXEL_FLOW_HANDLE_HPP
 
-#include <VoxFlow/Core/Utils/RendererCommon.hpp>
+#include <VoxFlow/Core/Utils/NonCopyable.hpp>
+#include <atomic>
+#include <memory>
 
 namespace VoxFlow
 {
@@ -11,16 +13,108 @@ namespace VoxFlow
 class HandleBase
 {
  public:
-    using HandleId = uint32_t;
+    using HandleID = uint64_t;
+    static constexpr HandleID InvalidHandleID = UINT64_MAX;
 
- protected:
-    HandleId _handleId;
+    HandleBase() : _handleId(InvalidHandleID){};
+    HandleBase(HandleBase::HandleID handleID) : _handleId(handleID){};
+    ~HandleBase(){};
+    HandleBase(const HandleBase& rhs)
+    {
+        operator=(rhs);
+    }
+    HandleBase& operator=(const HandleBase& rhs)
+    {
+        if (this != &rhs)
+        {
+            _handleId = rhs._handleId;
+        }
+        return *this;
+    }
+    HandleBase(HandleBase&& rhs)
+    {
+        operator=(std::move(rhs));
+    }
+    HandleBase& operator=(HandleBase&& rhs)
+    {
+        if (this != &rhs)
+        {
+            _handleId = rhs._handleId;
+            rhs._handleId = InvalidHandleID;
+        }
+        return *this;
+    }
+
+    virtual bool isValid() const
+    {
+        return _handleId != InvalidHandleID;
+    }
+
+    inline HandleID getHandleID() const
+    {
+        return _handleId;
+    }
+
+ private:
+    HandleID _handleId;
 };
 
 template <typename Type>
 class Handle : public HandleBase
 {
  public:
+    Handle() : HandleBase(HandleBase::InvalidHandleID)
+    {
+    }
+
+    Handle(std::weak_ptr<Type>&& obj) : _object(std::move(obj))
+    {
+        static std::atomic_uint64_t sAtomicHandleID(0ULL);
+        _handleId = sAtomicHandleID.fetch_add(1ULL);
+    }
+
+    Handle(const Handle& rhs)
+    {
+        operator=(rhs);
+    }
+
+    Handle& operator=(const Handle& rhs)
+    {
+        if (this != &rhs)
+        {
+            _object = rhs._object;
+            HandleBase::operator=(rhs);
+        }
+        return *this;
+    }
+    
+    Handle(Handle&& rhs)
+    {
+        operator=(std::move(rhs));
+    }
+    
+    Handle& operator=(Handle&& rhs)
+    {
+        if (this != &rhs)
+        {
+            _object = std::move(rhs._object);
+            HandleBase::operator=(std::move(rhs));
+        }
+        return *this;
+    }
+
+    inline bool isValid() const
+    {
+        return _object.expired() && HandleBase::isValid();
+    }
+
+    inline Type* getObjectPtr() const
+    {
+        return _object.get();
+    }
+
+ private:
+    std::weak_ptr<Type> _object;
 };
 
 }  // namespace VoxFlow
